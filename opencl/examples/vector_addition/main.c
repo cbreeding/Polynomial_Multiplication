4c86d59fca5aed998c23dd2fdf5cf1ab639a7ee5
Nick Miller <nick.miller.83@gmail.com>
2011-09-21 21:45:27 -0400

    Added an examples folder with two OpenCL examples

---
+++ opencl/examples/vector_addition/main.c
@@ -0,0 +1,132 @@
+//
+// NOTE: This example program is taken from 
+// http://www.thebigblob.com/getting-started-with-opencl-and-gpu-computing/
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <CL/cl.h>
+#include <time.h>
+ 
+#define MAX_SOURCE_SIZE (0x100000)
+ 
+int main(int argc, char* argv[]) 
+{
+    int i;
+    const unsigned int LIST_SIZE = 10000000;
+    
+    srand(time(NULL));
+    
+    // Create the input and output vectors
+    int *A = (int*)malloc(sizeof(int)*LIST_SIZE);
+    int *B = (int*)malloc(sizeof(int)*LIST_SIZE);
+    int *C = (int*)malloc(sizeof(int)*LIST_SIZE);
+    for(i = 0; i < LIST_SIZE; i++) 
+    {
+        //A[i] = i;
+        //B[i] = LIST_SIZE - i;
+        A[i] = rand() % 100;
+        B[i] = rand() % 100;
+    }
+ 
+    // Load the kernel source code into the array source_str
+    FILE *fp;
+    char *source_str;
+    size_t source_size;
+ 
+    fp = fopen("vector_add_kernel.cl", "r");
+    if (!fp) 
+    {
+        fprintf(stderr, "Failed to load kernel.\n");
+        exit(1);
+    }
+    source_str = (char*)malloc(MAX_SOURCE_SIZE);
+    source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp);
+    fclose( fp );
+ 
+    // Get platform and device information
+    cl_platform_id platform_id = NULL;
+    cl_device_id device_id = NULL;   
+    cl_uint ret_num_devices;
+    cl_uint ret_num_platforms;
+    cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+    ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_GPU, 1, 
+            &device_id, &ret_num_devices);
+ 
+    // Create an OpenCL context
+    cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+ 
+    // Create a command queue
+    cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+ 
+    // Create memory buffers on the device for each vector 
+    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
+            LIST_SIZE * sizeof(int), NULL, &ret);
+    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
+            LIST_SIZE * sizeof(int), NULL, &ret);
+    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
+            LIST_SIZE * sizeof(int), NULL, &ret);
+ 
+    // Copy the lists A and B to their respective memory buffers
+    ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
+            LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
+    ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, 
+            LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
+ 
+    // Create a program from the kernel source
+    cl_program program = clCreateProgramWithSource(context, 1, 
+            (const char **)&source_str, (const size_t *)&source_size, &ret);
+ 
+    // Build the program
+    ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+ 
+    // Create the OpenCL kernel
+    cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
+ 
+    // Set the arguments of the kernel
+    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
+    ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
+    
+    // Perform the test sequentially first, for comparison
+    clock_t start = clock();
+    for (i = 0; i < LIST_SIZE; i++)
+       C[i] = A[i] + B[i]; 
+    printf("n = %d, CPU execution took %.9f seconds\n", 
+      LIST_SIZE,
+      ((double)clock() - start) / CLOCKS_PER_SEC);
+ 
+    // Execute the OpenCL kernel on the list
+    start = clock();
+    size_t global_item_size = LIST_SIZE; // Process the entire lists
+    size_t local_item_size = 1; // Process one item at a time
+    ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
+            &global_item_size, &local_item_size, 0, NULL, NULL);
+ 
+    // Read the memory buffer C on the device to the local variable C
+    ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, 
+            LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
+   
+    printf("n = %d, GPU execution took %.9f seconds\n", 
+      LIST_SIZE,
+      ((double)clock() - start) / CLOCKS_PER_SEC);
+ 
+    // Display some results to the screen
+    //for(i = 0; i < LIST_SIZE; i++)
+    for(i = 0; i < 10; i++)
+        printf("%d + %d = %d\n", A[i], B[i], C[i]);
+ 
+    // Clean up
+    ret = clFlush(command_queue);
+    ret = clFinish(command_queue);
+    ret = clReleaseKernel(kernel);
+    ret = clReleaseProgram(program);
+    ret = clReleaseMemObject(a_mem_obj);
+    ret = clReleaseMemObject(b_mem_obj);
+    ret = clReleaseMemObject(c_mem_obj);
+    ret = clReleaseCommandQueue(command_queue);
+    ret = clReleaseContext(context);
+    free(A);
+    free(B);
+    free(C);
+    return 0;
+}
---
+++ opencl/examples/vector_addition/vector_add_kernel.cl
@@ -0,0 +1,12 @@
+//
+// NOTE: This example program is taken from 
+// http://www.thebigblob.com/getting-started-with-opencl-and-gpu-computing/
+//
+__kernel void vector_add(__global const int *A, __global const int *B, __global int *C) 
+{
+    // Get the index of the current element to be processed
+    int i = get_global_id(0);
+ 
+    // Do the operation
+    C[i] = A[i] + B[i];
+}
