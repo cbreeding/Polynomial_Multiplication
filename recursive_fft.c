eba925215588b94b0407cae703cfc57322079d35
Nick Miller <nick.miller.83@gmail.com>
2011-09-24 11:33:16 -0400

    (from Coty) Added debug print statements in recursive_fft.c

--- recursive_fft.c
+++ recursive_fft.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "common_defs.h"
 
 /* recursive_fft - see common_defs.h for more details */
@@ -11,14 +12,18 @@ void recursive_fft(complex* a, complex* y, int n, int inv)
    complex* y0;
    complex* y1;
    int i, k;
-   
+
    /* Base Case */
    if (n == 1)
    {
+      printf("     N = %d \n     ",n); /* Debug print of recursion trace, extra space in front if n=1 */
       y[0] = a[0];
       return;
    }
    
+   /* Debug print of recursion trace */
+   printf("N = %d \n     ",n);
+
    /* Calculate principal nth root of unity (i.e. exp(2*PI*i/n)) */
    if (inv)
    {
@@ -45,7 +50,7 @@ void recursive_fft(complex* a, complex* y, int n, int inv)
       a0[i] = a[2*i];
       a1[i] = a[2*i+1];
    }
-   
+
    /* Calculate 2 FFTs of size n/2 */
    recursive_fft(a0, y0, n/2, inv);
    recursive_fft(a1, y1, n/2, inv);
